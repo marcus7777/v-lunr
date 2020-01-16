aa9d617efa7a70541fc5e6523e556e769c3132b4
marcus <marcus@hhost.me>
2020-01-16 15:27:22 +0000

    just working.....

--- index.vue
+++ index.vue
@@ -14,42 +14,44 @@ export default {
   data: () => {
     return {}
   },
-  computed: {
-    output: function () {
-      if (this.input[0]){
+  watch: {
+    input: function (val) {
+      if (val[0]){
         let documents = this.input.map(function (val, i){
           return Object.assign({__id: i},val)
         })
         let first = this.input[0]
-        let idx = lunr(function () {
+        this.idx = lunr(function () {
           this.ref('__id')
           Object.keys(first).forEach(function (key) {
             this.field(key)
           }, this)
-
           documents.forEach(function (doc) {
             this.add(doc)
           }, this)
         })
-        return idx.search(this.search).map(function (val){
-          return documents[+val.ref]
-        })
-      } else {
-        return []
       }
     },
+    search: function(val) {
+      this.onUpdate(idx.search(val).map(function (valu){
+        return this.input[+valu.ref];
+      },this);
+    }
   },        
   props: {
-    input: { 
+    input: {
       type: Array,
     },
     search:{
       type: String,
       default: '',
     },
+    update:{
+      type: Function
+    }
   },
-  mounted() {
+  methods: {
     
-  },
+  }
 }
 </script>
