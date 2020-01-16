c0a147cf073fc6f9cea14626626b70cc8afd2e64
marcus <marcus@hhost.me>
2020-01-16 13:34:46 +0000

    starting

---
+++ index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    output {{output}}
+  </div>
+</template>
+
+<script>
+import lunr from 'lunr';
+
+export default {
+  name:  'vLunr',
+  components: {
+  },
+  data: () => {
+    return {}
+  },
+  computed: {
+    output: function () {
+      return this.input
+    },
+  },	
+  props: {
+    input: { 
+      default: [{1: "one"},{2: "two"},{3: "three"}],
+      required: true,
+      type: Array,
+    },
+    search:{
+      type: String,
+      default: '',
+    },
+  },
+  mounted() {
+    
+  },
+}
+</script>
