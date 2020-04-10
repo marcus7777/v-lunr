55558854454ad16c242b8fedbc0a66793a26a0ea
Marcus Hitchins <marcus7777@gmail.com>
2020-04-10 19:00:05 +0100

    Hopefully allowing for changes

--- index.vue
+++ index.vue
@@ -16,6 +16,46 @@ export default {
   mounted(){
   },
   computed:{
+    idx: function () {	
+      if (this.input[0]){	
+        const first = this.input[0]	
+        const stopWords = this.stopWords	
+        const documents = this.input.map(function (val, i){	
+          let doc = {}	
+          let seen = []	
+          function replacer(key, value) { 	
+            if (key[0] === '_') {	
+              return	
+            } else if (typeof value === 'object' && value !== null) {	
+              if (seen.indexOf(key) !== -1) return;	
+              else seen.push(key);	
+            }	
+            return value;	
+          }	
+          Object.keys(val).forEach(function(key) {	
+            doc[key] = JSON.stringify(val[key], replacer)	
+          })	
+          return Object.assign({__id: i}, val)	
+        })	
+        return lunr(function () {	
+          this.ref('__id')	
+          if (!stopWords) {	
+            this.pipeline.remove(lunr.stopWordFilter)	
+            this.pipeline.remove(lunr.stemmer)	
+          }	
+          Object.keys(first).forEach(function (key) {	
+            if (key[0] !== '_') {	
+              this.field(key)	
+            }	
+          }, this)	
+          documents.forEach(function (doc) {	
+            this.add(doc)	
+          }, this)	
+        })	
+      } else {	
+        return {}	
+      }	
+    },
     output: function(){
       if (this.search) {
         const output = this.idx.search(this.search).map(function (valu){
@@ -43,51 +83,6 @@ export default {
       type: Boolean,
       default: false,
     },
-    idx: {
-      default: function () {
-        if (this.input[0]){
-          const first = this.input[0]
-          const stopWords = this.stopWords
-          const documents = this.input.map(function (val, i){
-            let doc = {}
-            let seen = []
-            function replacer(key, value) { 
-              if (key[0] === '_') {
-                return
-              } else if (typeof value === 'object' && value !== null) {
-                if (seen.indexOf(key) !== -1) {
-                  return
-                } else {
-                  seen.push(key)
-                }
-              }
-              return value;
-            }
-            Object.keys(val).forEach(function(key) {
-              doc[key] = JSON.stringify(val[key], replacer)
-            })
-            return Object.assign({__id: i}, val)
-          })
-          return lunr(function () {
-            this.ref('__id')
-            if (!stopWords) {
-              this.pipeline.remove(lunr.stopWordFilter)
-              this.pipeline.remove(lunr.stemmer)
-            }
-            Object.keys(first).forEach(function (key) {
-              if (key[0] !== '_') {
-                this.field(key)
-              }
-            }, this)
-            documents.forEach(function (doc) {
-              this.add(doc)
-            }, this)
-          })
-        } else {
-          return {}
-        }
-      },
-    }
   },
 }
 
