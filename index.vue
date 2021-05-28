d1e5d6bde04ee3c8fad740a131e6a5a479d9afb6
Marcus Hitchins <marcus@hhost.me>
2021-05-28 18:34:14 +0100

    no local index just in mem

--- index.vue
+++ index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div v-for="(item, key) in output" :key="perpendKey+hashInput+key">
+    <div v-for="(item, key) in theOutput" :key="perpendKey+hashInput+key">
       <slot v-bind:item="item, key">
         {{ item }}
       </slot>
@@ -9,12 +9,73 @@
 </template>
 
 <script>
-import lunr from 'lunr';
+import lunr from 'lunr'
 
 export default {
   methods:{
+    output(search){
+      let that = this
+      if (this.idx && this.idx.search) {
+        return this.idx.search(search).map(function (valu){
+          return that.input[+valu.ref];
+        }, that)
+      }
+      if (this.idx.then) return this.idx.then(index => {
+        return index.search(search).map(function (valu){
+          return that.input[+valu.ref];
+        }, that)
+      })
+      // no index
+      return this.input
+    },
+    makeIndex(hashInput){
+      let that = this
+      if (this.input[0] && this.search){
+        const first = this.fields
+        if (this.log) console.log("feilds from ",first)
+
+        const stopWords = this.stopWords
+        const documents = this.input.map(function (val, i){
+          let doc = {}
+          let seen = []
+          function replacer(key, value) {
+            if (key[0] === '_') {
+              return
+            } else if (typeof value === 'object' && value !== null) {
+              if (seen.indexOf(key) !== -1) return;
+              else seen.push(key);
+            }
+            return value;
+          }
+          Object.keys(val).forEach(function(key) {
+            doc[key] = JSON.stringify(val[key], replacer)
+          })
+          return {__id: i, ...val}
+        })
+        const idx = lunr(function () {
+          this.ref('__id')
+          if (!stopWords) {
+            this.pipeline.remove(lunr.stopWordFilter)
+            this.pipeline.remove(lunr.stemmer)
+          }
+          Object.keys(first).forEach(function (key) {
+            if (key[0] !== '_') {
+              this.field(key)
+            }
+          }, this)
+          documents.forEach(function (doc) {
+            this.add(doc)
+          }, this)
+          if (that.log) console.log(this)
+        })
+        that.cache[hashInput] = idx.toJSON()
+        return idx
+      } else {
+        return {}
+      }
+    },
     hashThis(message, n = 8) {
-      if (n === 0) return message
+      if (+n === 0) return message
       let hash = 0
       if (message.length == 0) return this.hashThis(this.hex32(hash),n-1)
       for (let i = 0;i < message.length;i++) {
@@ -35,94 +96,37 @@ export default {
       return this.hashThis(JSON.stringify(this.input))
     },
     idx() {
-      const that = this
-      const hashInput = this.hashInput
-      const loaded = localStorage.getItem("lunr"+hashInput)
-
-      if (loaded){
-        return lunr.Index.load(JSON.parse(loaded))
-      } else if (this.input[0] && this.search){
-        const first = this.fields
-        if (this.log) console.log("feilds from ",first)
-
-        const stopWords = this.stopWords
-        const documents = this.input.map(function (val, i){
-          let doc = {}
-          let seen = []
-          function replacer(key, value) {
-            if (key[0] === '_') {
-              return
-            } else if (typeof value === 'object' && value !== null) {
-              if (seen.indexOf(key) !== -1) return;
-              else seen.push(key);
-            }
-            return value;
-          }
-          Object.keys(val).forEach(function(key) {
-            doc[key] = JSON.stringify(val[key], replacer)
-          })
-          return Object.assign({__id: i}, val)
-        })
-        const idx = lunr(function () {
-          this.ref('__id')
-          if (!stopWords) {
-            this.pipeline.remove(lunr.stopWordFilter)
-            this.pipeline.remove(lunr.stemmer)
-          }
-          Object.keys(first).forEach(function (key) {
-            if (key[0] !== '_') {
-              this.field(key)
-            }
-          }, this)
-          documents.forEach(function (doc) {
-            this.add(doc)
-          }, this)
-          if (that.log) console.log(this)
-        })
-        localStorage.setItem("lunr"+hashInput, JSON.stringify(idx) )
-        return idx
-      } else {
-        return {}
-      }
-    },
-    words(){
-      if (this.idx.invertedIndex) {
-        return Object.keys(this.idx.invertedIndex)
-      } else {
-        return []
-      }
-    },
-    output(){
-      if (this.search.trim()) {
-        const output = this.idx.search(this.search).map(function (valu){
-          return this.input[+valu.ref];
-        },this)
-        this.$emit("results", output)
-
-        return output
-      } else {
-        return this.input
-      }
-    },
-    outputAsText(){
-      return JSON.stringify(this.output)
+      if (!this.input[0]) return {}
+      const loaded = this.cache[this.hashInput]
+      return loaded ? lunr.Index.load(loaded) : this.makeIndex(this.hashInput)
     },
   },
   watch:{
-    output(output){
-      const hash = this.hashThis(this.outputAsText)
-      if (this.outputHash !== hash) {
-        this.outputHash = hash
-        this.$emit("results-length", output.length)
-        this.$emit("results", output)
-      }
-    },
-    words(words){
-      const hash = this.hashThis(JSON.stringify(words))
-      if (this.wordsHash !== hash) {
-        this.wordsHash = hash
-        this.$emit("words", words)
-      }
+    search:{
+      handler(search = ""){
+        if (!search.trim() || search == "undefined") {
+          this.$emit("results-length", this.input.length)
+          this.$emit("results", this.input)
+          this.theOutput = this.input
+          return
+        }
+        const update = output => {
+          const hash = this.hashThis(JSON.stringify(output))
+          if (this.outputHash !== hash) {
+            this.outputHash = hash
+            this.$emit("results-length", output.length)
+            this.$emit("results", output)
+            this.theOutput = output
+          }
+        }
+        if (this.output.then) {
+          return this.output(search).then(async output => {
+            update(await output)
+          })
+        }
+        update(this.output(search))
+      },
+      immediate: true,
     },
   },
   props: {
@@ -155,8 +159,9 @@ export default {
   },
   data(){
     return {
-      outputHash:"",
-      wordsHash:"",
+      outputHash: "",
+      theOutput:[],
+      cache: {},
     }
   },
 }
